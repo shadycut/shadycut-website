d5e6d8d72c507ea34d8fb51789ca6c3d19ccd423
ShadyFox <shadyfoxgg@gmail.com>
2026-09-17 00:40:46 +0200

    video/image opti, robots, sitemap, dead assets

--- components/loader.js
+++ components/loader.js
@@ -9,6 +9,7 @@ async function loadComponent(selector, path) {
 Promise.all([
   loadComponent('[data-component="header"]', 'components/header.html'),
   loadComponent('[data-component="case-study-cta"]', 'components/case-study-cta.html'),
+  loadComponent('[data-component="case-study-permission-note"]', 'components/case-study-permission-note.html'),
   loadComponent('[data-component="follow-the-journey"]', 'components/follow-the-journey.html'),
   loadComponent('[data-component="footer"]', 'components/footer.html')
 ]).then(() => {
